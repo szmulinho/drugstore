cfbb042308bb1149c4806bcb1c522cd7c2ae7909
szmulinho <samik1997@gmail.com>
2023-12-04 17:12:00 +0100

    get all

--- internal/server/endpoints/getAllDrugs.go
+++ internal/server/endpoints/getAllDrugs.go
@@ -2,15 +2,17 @@ package endpoints
 
 import (
 	"encoding/json"
-	"github.com/szmulinho/common/model"
+	"github.com/szmulinho/drugstore/internal/model"
 	"net/http"
 )
 
 func (h *handlers) GetAllDrugs(w http.ResponseWriter, r *http.Request) {
-	if err := h.db.Find(&model.Drugs{}).Error; err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+	result := h.db.Find(&model.Drugs)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	json.NewEncoder(w).Encode(model.Drugs{})
+	json.NewEncoder(w).Encode(model.Drugs)
 }
